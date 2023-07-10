6a6ddea25f270c5d90223087a0eae518011fb71d
iMad <damianos.imad@gmail.com>
2023-07-10 16:15:29 +0300

    first init

--- Marvel/HomeViewController.swift
+++ Marvel/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  Marvel
 //
 //  Created by iMad on 10/07/2023.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
